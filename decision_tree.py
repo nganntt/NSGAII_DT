6ea8b5b879389d63e4de1e915c07089493337b99
nganntt <32063558+nganntt@users.noreply.github.com>
2019-07-31 15:20:35 +0200

    Add files via upload

--- decision_tree.py
+++ decision_tree.py
@@ -1,9 +1,12 @@
-
+import sys_output
 import generate_data
 import pandas as pd
 import numpy as np
 import re
+import os
+from datetime import datetime
 
+from NSGAII import file_name
 
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
 from sklearn.model_selection import train_test_split # Import train_test_split function
@@ -15,6 +18,8 @@ import pydotplus
 from sklearn.datasets import load_iris
 from sklearn import tree
 import collections
+
+
 
 
 def estimate_critical(dist, speed):
@@ -94,28 +99,84 @@ def label_encode_distance(speed_list):
     return le.transform(dist_list) 
 
 
+#dist_bw_20_30  dist_bw_30_40  dist_gt_40  dist_lt_20  speed_bw_20_40  speed_bw_40_60  speed_gt_60  state
+   
+def check_dataFrame(df):
+           
+    df1 = pd.DataFrame()
+    data_frame = pd.DataFrame()
+    list_missing_colum = []
+    if 'dist_bw_20_30' not in df:
+        list_missing_colum.append('dist_bw_20_30')
+                       
+    if 'dist_bw_30_40' not in df:
+        list_missing_colum.append('dist_bw_30_40')
+   
+    if 'dist_gt_40' not in df:
+        list_missing_colum.append('dist_gt_40')
+
+    if 'dist_lt_20' not in df:
+        list_missing_colum.append('dist_lt_20')
+        # df1['dist_lt_20'] = np.zeros(len(df.index))
+        # data_frame = pd.concat([df,df1['dist_lt_20']],axis=1,sort=False)
+
+    if 'speed_bw_20_40' not in df:
+        list_missing_colum.append('speed_bw_20_40')
+    
+    if 'speed_bw_40_60' not in df:
+        list_missing_colum.append('speed_bw_40_60')
+       
+
+    if 'speed_gt_60' not in df:
+        list_missing_colum.append('speed_gt_60')
+     
+    for colume in list_missing_colum:
+        df1[colume] = np.zeros(df.shape[0])
+    
+    data_frame = pd.concat([df,df1],axis=1,sort=False)
+    
+    
+    
+    
+    return data_frame
+
+def file_name1():
+    frame = inspect.stack()[1]
+    module = inspect.getmodule(frame[0])
+    filename = module.__file__
+    return filename
+
 def convert_dataFrame(pop):
     """
     This function convers data from population to dataFrame for decision tree
     this function return a table of data
     """
-    #get data from population
+    #print ("population",pop)
     data = pop_to_data_DT(pop)
+    
+   # print ("pop to data",data)
     data_dt = []
     
     #encode data 
     i = 0
+    
+    print("\n\nEncode data from (car1_pos_rot, car2_pop_rot, speed) to (Distance_2cars, speed_behind_Car,state)")
+          # \n when Distance < 20 and speed >20 could  lead a crash of two car \n")
     for item in data:
         dist =  encode_dist(item[0])
         speed = encode_speed(item[1])
         state = item[2]
         data_dt.append((dist,speed,state))
-
+        print(item)
+    
+    #print data out
+    print("a. Take population and convert it to distance and speed for calculation of DT  \n \n ")
+    
+    sys_output.print_collection(data_dt,len(data_dt))
+   
     #conver to dataFrame 
     dfObj_tmp = pd.DataFrame(data_dt) 
    
-     #adding index for dataframe
-    #dfObj.index = [i for i in range (1,len(pop)+1)]
   
     dfObj = dfObj_tmp.set_axis(['distance', 'speed', 'state'], axis=1, inplace=False)
  
@@ -124,11 +185,11 @@ def convert_dataFrame(pop):
     
     #data has format
     #dist_bw_20_30  dist_bw_30_40  dist_gt_40  dist_lt_20  speed_bw_20_40  speed_bw_40_60  speed_gt_60  state
-    data_training_DT = pd.concat([one_hot_data_dist,one_hot_data_speed,dfObj['state']],axis=1,sort=False)
-    # print("distance ",one_hot_data_dist.head(5))
-    # print("speed ",one_hot_data_speed.head(5))
-    # print("all ",data_training_dt.head(10))
-    #id = [i for i in range (1,len(pop)+1)]
+    data_training_DT_temp = pd.concat([one_hot_data_dist,one_hot_data_speed,dfObj['state']],axis=1,sort=False)
+
+    data_training_DT = check_dataFrame(data_training_DT_temp)
+    print ("\n Convert data to dataframe to train DT \n", data_training_DT )
+    
     return data_training_DT
 
 
@@ -140,72 +201,75 @@ def get_critical_sample_DT(df):
     feature_name = ['dist_bw_20_30', 'dist_bw_30_40', 'dist_gt_40', 'dist_lt_20', \
                    'speed_bw_20_40', 'speed_bw_40_60', 'speed_gt_60']
     feature_cols = feature_name
+
     X = df[feature_cols] # Features
     y = df['state'] # Target variable
 
     
     clf = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=None,
                                  max_features=None, max_leaf_nodes=None,
-                                 min_impurity_split=1e-07, min_samples_leaf=1,
+                                 min_impurity_decrease=1e-07, min_samples_leaf=1,
                                  min_samples_split=2, min_weight_fraction_leaf=0.0,
                                  presort=False, random_state=None, splitter='best')
 
     # Train Decision Tree Classifer
     #clf = clf.fit(X_train,y_train)
+    
     clf = clf.fit(X,y)
     visualize_data(clf,feature_name)
-    
-  
     n_nodes = clf.tree_.node_count
-    rules, path_values = get_rules(clf,df)
-    print("riles", rules)
-    print("path",path_values)
-    filter_index_df = [] 
-    #get index of sample in collision node
-    # path_value is a list of numpy array
-    for  idx, path_value in  enumerate(path_values):
+   
+    children_left = clf.tree_.children_left
+    children_right = clf.tree_.children_right
+    feature = clf.tree_.feature
+    threshold = clf.tree_.threshold
+    print ("**************************** DT *******************************")
+    print("children_left : ", children_left)
+    print("children_right : ", children_right)
+    print("feature : ", feature)
+    print("threshold : ", threshold)
        
-        if check_to_get_node_coll(path_value):
-            items = eval(to_code(var_df,rules[idx]))
-            for index,row in items.iterrows():
-                filter_index_df.append(index)
+    filter_index_df = [] 
+    #consider 2 case: there are only one node and there are many nodes
+    if n_nodes > 1: 
+        rules, path_values = get_rules(clf,df)
+        # print information about tree 
+        print ("\n     Number node of Tree:", n_nodes)
+        print ("\n     List of rules in the Tree:\n ")
+        sys_output.print_collection(rules,len(rules))
+        print("\n      Number [safety_scenario, critical_scenario] in each node of tree:\n")
+        print(clf.tree_.value)
+        
+        #get index of sample in collision node
+        # path_value is a list of numpy array
+        print ("\n     Check nodes that critical events are larger than 50% on total Scenarios\n ")
+        print ("\n     List of nodes is selected in Decision Tree:")
+        counter = 0
+
+        for  idx, path_value in  enumerate(path_values):
+            
+            if check_to_get_node_coll(path_value):
+                counter += 1
+                print ("     ", path_value[len(path_values) -1])
+                items = eval(to_code(var_df,rules[idx]))
+                print ("\n Select data from leaf node: \n %s" %items)
+                for index,row in items.iterrows():
+                    filter_index_df.append(index)
+    else:
+        sys_output.warning("There is only one node in decision tree. \
+                \nThus, NSGAII could not explore critical events from Decision Tree ")
+       
+        filter_index_df = [i for i in range(df.shape[0])]
+        print("\n Data frame \n")
+        for i in df:
+            print (i)
+            
     # return index of population
     return filter_index_df       
-    
-    #print("index ",filter_index_df)           
-            
-    
-    # a,b = get_rules(clf,df)
-    # for i in a:
-        # #print( "encode :",   to_code("df",i))
-        # v = eval(to_code("df",i))
-        # list_index = []
-        # for index,row in v.iterrows():
-            # list_index.append(index)
-        # #print(list_index)
-       # # print(v)
-        # #print("get rule ",i)
-    # for i in b:
-        # print("type",type(i))
-        # print("value",i)
-        # print("shape",i[0].shape)
-        # print("item 1 ",i[0][:,0])
-        # print("item 2 ",i[0][:,1])
-        # #print("shape : ",  i.shape )
-        
-   
-        
-    
-   
-   
-    
 
-    
     
 def visualize_data (clf,data_feature_names):
     # Visualize data
-    
-                                
                                 
     dot_data = tree.export_graphviz(clf,
                                 feature_names=data_feature_names,
@@ -226,14 +290,23 @@ def visualize_data (clf,data_feature_names):
             dest = graph.get_node(str(edges[edge][i]))[0]
             dest.set_fillcolor(colors[i])
 
-    graph.write_png('tree.png')
+    #save tree
+    path_dir = os.getcwd()   
+    now = datetime.now()    
+    date_time = now.strftime("%m_%d_%Y__%H%M%S")
+    
+    graph.write_png(path_dir +"\store\\tree_" + date_time+".png")
+    sys_output.print_sub_tit("The graph of tree is save in " + path_dir +"\store\\tree_" + date_time+".png" )
 
     
-
 def get_rules(dtc, df):
+    """
+    This function takes rule from decision tree
+    """
     rules_list = []
     values_path = []
     values = dtc.tree_.value
+   
 
     def RevTraverseTree(tree, node, rules, pathValues):
         '''
@@ -296,11 +369,6 @@ def get_rules(dtc, df):
         #nodevalue = dtc.tree_.value[nod]
 
     return (rules_list, values_path)
-    
-    
-
-    
-
 
     
     
@@ -327,33 +395,39 @@ def to_code (var_name_df, rules):
                 string_rule += " & ("+ var_name_df +'["' + str(feature) + '"] ' + str(oper) + ' ' + str(value) + " )"
     
     string_to_cod = var_name_df + '[ ' +string_rule + ']'
+    
+    print ("#########query",string_to_cod )
     return string_to_cod
 
+
+def get_max_critical (tree_nodes):
+    """
+    The function will return the node which has max value of critical event (except root node)
+    """
+    _max = 0
+    dim = tree_nodes.shape
+    if (tree_nodes.size >1):
+        list_critical = np.zeros(dim[0]-1)
+    
+        #don't get the first node from the tree
+   
+        for i in range(1, dim[0]):
+            list_critical[i-1] = tree_nodes[i][0][1]
+        
+    _max = np.argmax(list_critical)
+    return _max
 
 def check_to_get_node_coll(path_list):
    
     
     last_id = len(path_list) -1
     value = path_list[last_id]
-
-    # print("value shape", value.shape)
-    # print(" check_to_get_node_coll value1",value[0:1][0][0])
-    # print(" check_to_get_node_coll value2",value[0:1][0][1])
     
     #number of collision bigger than number of safe scenarios
     if value[0:1][0][0] < value[0:1][0][1]:
         return 1
     else:
         return 0
-
-
-
-
-for i in range (10,70):
-    pass
-    #print (i, encode_speed(i), encode_dist(i))
-    
-   # print (a)
 
 
    
